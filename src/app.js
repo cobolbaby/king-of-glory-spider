56b96979f267bce11999dc199e62b851a83b20a8
steve liao <mynamelky@gmail.com>
2017-08-04 23:58:18 +0800

    update spider module

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const schedule = require('node-schedule');
+
 /**
  * Created by kaeyleo on 2017-07-28.
  * 
@@ -9,10 +11,12 @@
 // 自定义模块
 const spider = require('./spider/index');
 
-// 定时爬取并保存周免英雄
-// spider.freeHero();
+// 每周一定时爬取并保存周免英雄
+schedule.scheduleJob('* * * * * 1', function(){
+    spider.freeHero();
+});
 
 // 爬取英雄详情信息
-spider.hero();
+// spider.hero(); 
 
 })();
