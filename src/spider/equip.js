b709ccd1fa9adf0a1d4aca880a93ba3ea2f3c750
steve liao <mynamelky@gmail.com>
2017-08-06 22:57:04 +0800

    update

--- src/spider/equip.js
+++ src/spider/equip.js
@@ -9,21 +9,27 @@ const equip_data = JSON.parse(fs.readFileSync('./data/item.json').toString());
  * 获取装备列表
  */
 function equip() {
-    rp({
-        url: utils.getEquipUrl(),
-        json: true
-    })
-    .then(function(res) {
-        for(let i in res) {
-            const item = res[i],
-                  item_id = item.item_id,
-                  item_img = `//game.gtimg.cn/images/yxzj/img201606/itemimg/${item_id}.jpg`;
-            
-            item.img = item_img;
+    storage.init();
 
-            console.log(item);
-        }
-    });
+    // rp({
+    //     url: utils.getEquipUrl(),
+    //     json: true
+    // })
+    // .then(function(res) {
+    //     for(let i in res) {
+    //         const item = res[i],
+    //               item_id = item.item_id,
+    //               item_img = `//game.gtimg.cn/images/yxzj/img201606/itemimg/${item_id}.jpg`;
+            
+    //         item.img = item_img;
+
+    //         storage.saveOrUpdate({ 
+    //             key: 'item_id', 
+    //             val: item_id 
+    //         }, 'Equip', item);
+    //     }
+    // });
+    console.log(equip_data.length);
 }
 
 module.exports = equip;
